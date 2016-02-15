36b92bfa369e0ed29d635306159ae83f98a7fd55
Derbeth <derbeth@interia.pl>
2016-02-15 20:55:56 +0100

    fixApostrophes1-3

--- src/ort.js
+++ src/ort.js
@@ -14,6 +14,9 @@
         _fixLine(line) {
             line = this._fixNumerals1(line);
 
+            line = this._fixApostrophes1(line);
+            line = this._fixApostrophes2(line);
+            line = this._fixApostrophes3(line);
             line = line.replace(/(Jak|Luk|Mik|[rR]emak|Spik)e('|’|`|-|–|—)(iem|em|m)\b/g, '$1iem');
             line = line.replace(/\[\[([^|\]]*(Luk|Mik|[rR]emak|Spik))e\s*\]\]('|’|`|-|–|—)(em|m)\b/g, '[[$1e|$1iem]]');
 
@@ -34,7 +37,7 @@
             if (matches) {
                 const m1 = matches[1];
                 let match = matches[0];
-                let before = this._prematch(line, matches);
+                const before = this._prematch(line, matches);
                 let after = this._postmatch(line, matches);
                 if (!/https?:\/\/\S+$|(Grafika|Image|Plik|File):[^\|]*$/i.exec(before)) {
                     match = `${m1}-`;
@@ -45,11 +48,64 @@
             return line;
         }
 
+        //Jay'a-Z
+        _fixApostrophes1(line) {
+            return this._safeReplace(line,
+                /((?:b|c|d|f|g|h|j|k|l|m|n|p|r|s|t|v|x|w|z|ey|ay|oy|uy|o|ee|i)]?]?)(?:'|’|`|-|–|—)(ach|iem|em|ów|owych|owym|owy|owego|owej|owe|owskimi|owskich|owskiego|owskie|owskim|owski|owcy|owca|owców|owie|owi|ową|ami|ie|ego|go|emu|om|ą|ę|a|i|e|y|mu|m|u)\b/,
+                (match, matches, before, after) => {
+                    if (!this.risky && after.match(/^-/) ||
+                    this._isLinkStart(before) ||
+                    `${before}${matches[1]}`.match(/(Barthes|Georges|Gilles|Jacques|Yves)$/)) {
+                        return match;
+                    }
+                    return `${matches[1]}${matches[2]}`;
+                }
+            );
+        }
+
+        // Laurie'mu -> Lauriemu
+        _fixApostrophes2(line) {
+            return this._safeReplace(line,
+                /((ie)]?]?)(?:'|’|`|-|–|—)(go|mu|m)\b(?!-)/,
+                (match, matches, before) => {
+                    if (this._isLinkStart(before)) {
+                        return match;
+                    }
+                    return `${matches[1]}${matches[3]}`;
+                }
+            );
+        }
+
+        // Selby'ch -> Selbych
+        _fixApostrophes3(line) {
+            return this._safeReplace(line,
+                /([y])(?:'|’|`|-|–|—)(ch)\b(?!-)/,
+                (match, matches, before) => {
+                    if (this._isLinkStart(before)) {
+                        return match;
+                    }
+                    return `${matches[1]}${matches[2]}`;
+                }
+            );
+        }
+
         _addMissingPolishAccents(line) {
             if (!this.risky) {
                 return line;
             }
             line = line.replace(/\b(imi|książ|mas|par|plemi|zwierz)e\b/, '$1ę');
+            return line;
+        }
+
+        _safeReplace(line, regex, matchCallback) {
+            const matches = regex.exec(line);
+            if (matches) {
+                const before = this._prematch(line, matches);
+                const after = this._postmatch(line, matches);
+                line = before
+                    + matchCallback(matches[0], matches, before, after)
+                    + this._safeReplace(after, regex, matchCallback);
+            }
             return line;
         }
 
@@ -59,6 +115,10 @@
 
         _postmatch(string, match) {
             return string.substring(match.index + match[0].length);
+        }
+
+        _isLinkStart(text) {
+            return text.match(/https?:\/\/\S+$|(Grafika|Image|Plik|File):[^\|]*$/i);
         }
     }
 
